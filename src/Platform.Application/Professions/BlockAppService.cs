63db2c8fedcb5d4f87061705888b23f8ce88e0e1
Illarionov Yegor <egorliberty@gmail.com>
2019-03-29 23:07:51 +0200

    cleanup

--- src/Platform.Application/Professions/BlockAppService.cs
+++ src/Platform.Application/Professions/BlockAppService.cs
@@ -13,59 +13,12 @@ namespace Platform.Professions
     {
         private readonly IRepository<Block, long> repository;
         private readonly IRepository<BlockTranslations, long> translationRepository;
-        public BlockAppService(IRepository<Block, long> blockRepository, IRepository<BlockTranslations, long> translationRepository)
+        public BlockAppService(IRepository<Block, long> blockRepository,
+            IRepository<BlockTranslations, long> translationRepository)
         {
             repository = blockRepository;
             this.translationRepository = translationRepository;
-        }
-
-        //public Task AddStep(AddStepDto input)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        public async Task AddTranslation(AddBlockTranslationDto input, long id)
-        {
-            var translation = ObjectMapper.Map(input, new BlockTranslations());
-            var block = await repository.GetAllIncluding(p => p.Translations)
-                .FirstOrDefaultAsync(p => p.Id == id);
-            block.Translations.Add(translation);
-        }
-
-        //public async Task<BlockReplyOkDto> CreateBlock(CreateBlockDto input)
-        //{
-        //    var block = ObjectMapper.Map<Block>(input);
-        //    var newid = await repository.InsertAndGetIdAsync(block);
-        //    return new BlockReplyOkDto { id = newid, message = "created" };
-        //}
-
-        //public async Task<BlockReplyOkDto> CreateCopy(long id)
-        //{
-        //    var block= await repository.GetAllIncluding(p => p.Translations)
-        //       .FirstOrDefaultAsync(p => p.Id == id);
-        //    var newblockdto = ObjectMapper.Map(block, new CreateBlockDto());
-        //    var newblock = ObjectMapper.Map(newblockdto, new Block());
-        //    var newid = await repository.InsertAndGetIdAsync(newblock);
-        //    return new BlockReplyOkDto { id = newid, message = "created" };
-        //}
-
-        public async Task<BlockReplyOkDto> DeleteBlock(long id)
-        {
-            var block = await repository.GetAllIncluding(p => p.Translations)
-                .FirstOrDefaultAsync(p => p.Id == id);
-            block.IsDeleted = true;
-            block.IsActive = false;
-            var res = await repository.InsertOrUpdateAndGetIdAsync(block);
-            return new BlockReplyOkDto { id = res, message = "deleted" };
-        }
-
-        public async Task DeleteTranslation(DeleteBlockTranslationDto input)
-        {
-            var ts = await repository.FirstOrDefaultAsync(p => p.Id == input.id);
-            ts.IsActive = false;
-            ts.IsDeleted = true;
-            await repository.InsertOrUpdateAsync(ts);
-        }
+        }   
 
         public async Task<GetBlockDto> GetBlock(long id)
         {
@@ -83,12 +36,7 @@ namespace Platform.Professions
             return res;
         }
 
-        //public Task RemoveStep(RemoveStepDto input)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        public  async Task<BlockReplyOkDto> UpdateBlock(UpdateBlockDto input)
+        public async Task<BlockReplyOkDto> UpdateBlock(UpdateBlockDto input)
         {
             var block = await repository.GetAllIncluding(p => p.Translations)
               .FirstOrDefaultAsync(p => p.Id == input.Id);
@@ -98,10 +46,46 @@ namespace Platform.Professions
             return new BlockReplyOkDto { id = res, message = "updated" };
         }
 
+        public async Task<BlockReplyOkDto> DeleteBlock(long id)
+        {
+            var block = await repository.GetAllIncluding(p => p.Translations)
+                .FirstOrDefaultAsync(p => p.Id == id);
+            block.IsDeleted = true;
+            block.IsActive = false;
+            var res = await repository.InsertOrUpdateAndGetIdAsync(block);
+            return new BlockReplyOkDto { id = res, message = "deleted" };
+        }
+
+        public async Task AddTranslation(AddBlockTranslationDto input, long id)
+        {
+            var translation = ObjectMapper.Map(input, new BlockTranslations());
+            var block = await repository.GetAllIncluding(p => p.Translations)
+                .FirstOrDefaultAsync(p => p.Id == id);
+            block.Translations.Add(translation);
+        }
+
         public async Task UpdateTranslation(UpdateBlockTranslationDto input)
         {
             var ts = ObjectMapper.Map<BlockTranslations>(input);
             await translationRepository.InsertOrUpdateAndGetIdAsync(ts);
         }
+
+        public async Task DeleteTranslation(DeleteBlockTranslationDto input)
+        {
+            var ts = await repository.FirstOrDefaultAsync(p => p.Id == input.id);
+            ts.IsActive = false;
+            ts.IsDeleted = true;
+            await repository.InsertOrUpdateAsync(ts);
+        }
+
+        //public Task AddStep(AddStepDto input)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task RemoveStep(RemoveStepDto input)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
--- src/Platform.Application/Professions/ProfessionAppService.cs
+++ src/Platform.Application/Professions/ProfessionAppService.cs
@@ -18,21 +18,13 @@ namespace Platform.Professions
         private readonly IRepository<ProfessionTranslations, long> _translationRepository;
         private readonly IRepository<Block, long> _blockRepository;
 
-        public ProfessionAppService(IRepository<Profession, long> professionRepository, IRepository<ProfessionTranslations, long> translationRepository, IRepository<Block, long> blockRepository)
+        public ProfessionAppService(IRepository<Profession, long> professionRepository, 
+            IRepository<ProfessionTranslations, long> translationRepository, 
+            IRepository<Block, long> blockRepository)
         {
             _professionRepository = professionRepository;
             _translationRepository = translationRepository;
             _blockRepository = blockRepository;
-        }
-
-        public async Task AddBlock(AddBlockDto input, long id)
-        {
-            var prof = await _professionRepository.FirstOrDefaultAsync(p => p.Id == id);
-            var block = ObjectMapper.Map<Block>(input);
-            var newid=await _blockRepository.InsertAndGetIdAsync(block);
-            var b= await _blockRepository.FirstOrDefaultAsync(p => p.Id == newid);
-            //prof.Blocks.Add(b);
-            b.Profession = prof;
         }
 
         public async Task AddTranslation(AddProfessionTranslationDto input, long id)
@@ -53,16 +45,6 @@ namespace Platform.Professions
 
             var newid = await _professionRepository.InsertAndGetIdAsync(newprof);
             return new ProfessionReplyOkDto { id = newid, message = "created" };
-        }
-
-        public async Task<ProfessionReplyOkDto> DeleteProfession(long id)
-        {
-            var prof = await _professionRepository.GetAllIncluding(p => p.Translations)
-                .FirstOrDefaultAsync(p => p.Id == id);
-            prof.IsDeleted = true;
-            prof.IsActive = false;
-            var res = await _professionRepository.InsertOrUpdateAndGetIdAsync(prof);
-            return new ProfessionReplyOkDto { id = res, message = "deleted" };
         }
 
         public async Task DeleteTranslation(DeleteProfessionTranslationDto input)
@@ -87,34 +69,27 @@ namespace Platform.Professions
         public async Task<ProfessionReplyOkDto> CreateProfession(ProfessionCreateDto input)
         {
             var prof = ObjectMapper.Map<Profession>(input);
-            //prof.
-            //  prof.CreatorUserId=
             var newid = await _professionRepository.InsertAndGetIdAsync(prof);
             return new ProfessionReplyOkDto { id = newid, message = "created" };
         }
-
-        //public async Task<IEnumerable<GetProfessionsDto>> GetProfessions()
-        //{
-        //    var profs = await _professionRepository.GetAllIncluding(p => p.Translations)
-        //       .ToListAsync();
-        //    var res = new List<GetProfessionsDto>();
-        //    foreach (var item in profs)
-        //    {
-        //        res.Add(ObjectMapper.Map(item, new GetProfessionsDto()));
-        //    }
-        //    return res;
-        //}
 
         public async Task<ProfessionReplyOkDto> UpdateProfession(ProfessionUpdateDto input)
         {
             var prof = await _professionRepository.GetAllIncluding(p => p.Translations)
                .FirstOrDefaultAsync(p => p.Id == input.Id);
-
             prof.Translations.Clear();
-
             var upd = ObjectMapper.Map(input, prof);
             var res = await _professionRepository.InsertOrUpdateAndGetIdAsync(upd);
             return new ProfessionReplyOkDto { id = res, message = "updated" };
+        }
+        public async Task<ProfessionReplyOkDto> DeleteProfession(long id)
+        {
+            var prof = await _professionRepository.GetAllIncluding(p => p.Translations)
+                .FirstOrDefaultAsync(p => p.Id == id);
+            prof.IsDeleted = true;
+            prof.IsActive = false;
+            var res = await _professionRepository.InsertOrUpdateAndGetIdAsync(prof);
+            return new ProfessionReplyOkDto { id = res, message = "deleted" };
         }
 
         public async Task<GetProfessionDto> GetProfession(long id)
@@ -133,27 +108,24 @@ namespace Platform.Professions
             return res;
         }
 
-        //public async Task<IEnumerable<GetProfessionAllDto>> GetProfessionsAll()
-        //{
-        //    var profs = await _professionRepository.GetAllIncluding(p => p.Translations)
-        //      .ToListAsync();
-        //    var res = new List<GetProfessionAllDto>();
-        //    foreach (var item in profs)
-        //    {
-        //        res.Add(ObjectMapper.Map(item, new GetProfessionAllDto()));
-        //    }
-        //    return res;
-        //}
 
         public async Task RemoveBlock(RemoveBlockDto input)
         {
             var prof = await _professionRepository.GetAllIncluding(p => p.Translations)
               .FirstOrDefaultAsync(p => p.Id == input.ProfessionId);
-            var block= await _blockRepository.GetAllIncluding(p => p.Translations)
+            var block = await _blockRepository.GetAllIncluding(p => p.Translations)
               .FirstOrDefaultAsync(p => p.Id == input.BlockId);
-            //prof.Blocks.Remove(block);
             block.IsActive = false;
             block.IsDeleted = true;
+        }
+
+        public async Task AddBlock(AddBlockDto input, long id)
+        {
+            var prof = await _professionRepository.FirstOrDefaultAsync(p => p.Id == id);
+            var block = ObjectMapper.Map<Block>(input);
+            var newid = await _blockRepository.InsertAndGetIdAsync(block);
+            var b = await _blockRepository.FirstOrDefaultAsync(p => p.Id == newid);
+            b.Profession = prof;
         }
     }
 }
