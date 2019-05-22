ae2d89f2df203f95b02c6cff4ab2cc1c5cd7e5b7
Illarionov Yegor <egorliberty@gmail.com>
2019-05-22 15:55:27 +0300

    stage

---
+++ src/Platform.EntityFrameworkCore/Migrations/20190519161656_up_softdelete.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Platform.Migrations
+{
+    public partial class up_softdelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "UserProfessions",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "UserProfessions");
+        }
+    }
+}
